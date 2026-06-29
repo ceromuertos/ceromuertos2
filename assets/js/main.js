8f89eee477ad6b508d33a6da881973701a10c527
ceromuertos <emmanuel@arizpe.com.mx>
2026-06-28 18:00:17 -0600

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,16 @@
+(() => {
+  const gate = document.getElementById('ageGate');
+  const accept = document.querySelector('[data-age-accept]');
+  const accepted = localStorage.getItem('okcm-age-confirmed') === 'yes';
 
+  if (gate && accepted) {
+    gate.classList.add('is-hidden');
+  }
+
+  if (accept) {
+    accept.addEventListener('click', () => {
+      localStorage.setItem('okcm-age-confirmed', 'yes');
+      gate?.classList.add('is-hidden');
+    });
+  }
+})();
