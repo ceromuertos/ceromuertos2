bed03122b92b1c148b26665b18a480c91f7bcd43
ceromuertos <emmanuel@arizpe.com.mx>
2026-07-09 15:45:06 -0600

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,9 @@
 (() => {
   const gate = document.getElementById('ageGate');
   const accept = document.querySelector('[data-age-accept]');
-  const accepted = localStorage.getItem('okcm-age-confirmed') === 'yes';
-
-  if (gate && accepted) {
+  if (gate && localStorage.getItem('okcm-age-confirmed') === 'yes') {
     gate.classList.add('is-hidden');
   }
-
   if (accept) {
     accept.addEventListener('click', () => {
       localStorage.setItem('okcm-age-confirmed', 'yes');
